e13bf17fcf39a93a7062989608bf8d4de280ad95
Ricardo Pinheiro <ricardopinheiro2008@gmail.com>
2023-12-14 17:45:35 -0300

    Fim da aula de template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def hello_blog(request):
+    lista = ['Django', 'Python', 'Git', 'Html', 'Banco de dados', 'Linux']
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias':lista}
+
+    #return HttpResponse("Blog")
+    return render(request, 'index.html', data)
