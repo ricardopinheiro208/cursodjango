e9b3629a070f5c11858632d88c3936c7a0193de9
Ricardo Pinheiro <ricardopinheiro2008@gmail.com>
2023-12-15 05:58:00 -0300

    aula banco de dados

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 # Create your views here.
 
 def hello_blog(request):
+    list_posts = Post.objects.all()
     lista = ['Django', 'Python', 'Git', 'Html', 'Banco de dados', 'Linux']
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias':lista}
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista, 'posts': list_posts }
 
     #return HttpResponse("Blog")
+    
     return render(request, 'index.html', data)
